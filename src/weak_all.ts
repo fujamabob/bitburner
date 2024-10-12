46aae1fd97d0c7ca8eaca781e808ad0ba4fe672f
fujamabob <fujamabob@gmail.com>
2024-10-11 21:40:07 -0500

    The hacking times are too damn high

---
+++ src/weak_all.ts
@@ -0,0 +1,20 @@
+import { NS } from "@ns";
+import { RPCClient } from "./lib/free/rpc";
+import { ServerInfo } from "./lib/free/server_info";
+
+export async function main(ns: NS): Promise<void> {
+    const rpc = new RPCClient(ns)
+    const servers = await rpc.call('get_server_list') as string[]
+    for (const server of servers) {
+        const info = await rpc.call('get_server_info', server) as ServerInfo
+        if ((info.moneyMax ?? 0) <= 0 || info.minDifficulty === undefined || !info.hasAdminRights)
+            continue
+        ns.print(`Weakening ${server}`)
+        const weak_amt = 0.05
+        const threads = Math.ceil((ns.getServerSecurityLevel(server) - info.minDifficulty) / weak_amt)
+        ns.tprint(`ns.run("weak.js", { preventDuplicates: true, threads: ${threads} }, ${server})`)
+        if (threads == 0)
+            continue
+        ns.run("weak.js", { preventDuplicates: true, threads: Math.min(threads, 64) }, server)
+    }
+}
