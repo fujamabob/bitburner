99c5119f8bcaf3a6510035f14ac007ba9de260ec
fujamabob <fujamabob@gmail.com>
2024-10-14 11:03:56 -0500

    Cleanup

--- src/bootstrap/goal3.ts
+++ src/bootstrap/goal3.ts
@@ -2,7 +2,7 @@ import { NS } from "@ns";
 import * as owned_server from "../lib/owned_server";
 
 export async function main(ns: NS): Promise<void> {
+    ns.disableLog('asleep')
     // Goal #3: Begin personal server acquisition
-    ns.tprint('Please buy some personal servers')
     await owned_server.manage(ns, 'fixer', 0.1)
 }
