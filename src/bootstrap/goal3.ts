17024959389fc77e8f6339ff7e13ec076ea8b850
fujamabob <fujamabob@gmail.com>
2024-10-23 10:17:34 -0500

    fixups

--- src/bootstrap/goal3.ts
+++ src/bootstrap/goal3.ts
@@ -3,6 +3,7 @@ import * as owned_server from "../lib/owned_server";
 
 export async function main(ns: NS): Promise<void> {
     ns.disableLog('asleep')
+    ns.disableLog('getServerMaxRam')
     // Goal #3: Begin personal server acquisition
     await owned_server.manage(ns, 'fixer', 0.1)
 }
