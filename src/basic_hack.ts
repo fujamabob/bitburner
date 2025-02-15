04aa6bdb1b375b7d40dace9f4b9f045cb4544ff3
fujamabob <fujamabob@gmail.com>
2025-02-15 13:41:46 -0600

    Basic scripts: do the thing until I say stop

---
+++ src/basic_hack.ts
@@ -0,0 +1,16 @@
+import { NS } from "@ns";
+import { init_script, Schema } from "./lib/utils";
+
+export async function main(ns: NS): Promise<void> {
+    const arg_schema = [
+        ['d', 0],    // Minimum time delta, in ms
+    ] as Schema
+    const [flags, args] = await init_script(ns, arg_schema)
+
+    const hostname = args[0] as string
+    for (; ;) {
+        await ns.hack(hostname, { additionalMsec: flags.d as number })
+        const time = new Date()
+        await ns.asleep(1000 - time.getMilliseconds())
+    }
+}
